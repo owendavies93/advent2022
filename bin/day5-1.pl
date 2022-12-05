ffcb4381d9387a6492603db1f1a99c0e50bb28dc
Owen Davies <owen@odavi.es>
2022-12-05 05:33:27 +0000

    Add Day 5

---
+++ bin/day5-1.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use Mojo::Base -strict;
+
+my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day5';
+$file = "inputs/day5-$file" if $file =~ /test/;
+
+my $stacks = [
+    [],
+    ['Z','N'],
+    ['M','C','D'],
+    ['P'],
+];
+
+if ($file !~ /test/) {
+    $stacks = [
+        [],
+        ['W','B','D','N','C','F','J'],
+        ['P','Z','V','Q','L','S','T'],
+        ['P','Z','B','G','J','T'],
+        ['D','T','L','J','Z','B','H','C'],
+        ['G','V','B','J','S'],
+        ['P','S','Q'],
+        ['B','V','D','F','L','M','P','N'],
+        ['P','S','M','F','B','D','L','R'],
+        ['V','D','T','R'],
+    ];
+}
+
+open(my $fh, '<', $file) or die $!;
+while (<$fh>) {
+    chomp;
+    my ($num, $from, $to) = $_ =~ /(\d+) from (\d) to (\d)/;
+    for (1..$num) {
+        my $val = pop @{$stacks->[$from]};
+        push @{$stacks->[$to]}, $val;
+    }
+}
+
+say map { $_->[-1] // '' } @$stacks;
