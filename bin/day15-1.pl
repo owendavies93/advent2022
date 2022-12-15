54197bbbbe0f717ff7a3799d466a1dfbcf9ae31e
Owen Davies <owen@odavi.es>
2022-12-15 12:29:13 +0000

    Add Day 15
    Slow!

---
+++ bin/day15-1.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use Mojo::Base -strict;
+
+use lib "../cheatsheet/lib";
+
+use Advent::Utils::Input qw(get_ints);
+
+use Array::Utils qw(:all);
+
+my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day15';
+$file = "inputs/day15-$file" if $file =~ /test/;
+
+my $target_row = $file =~ /test/ ? 10 : 2000000;
+my $no_b_xs = {};
+
+open(my $fh, '<', $file) or die $!;
+while (<$fh>) {
+    chomp;
+    my ($sx, $sy, $bx, $by) = get_ints($_, 1);
+    my $d = distance($sx, $sy, $bx, $by);
+    my ($min, $max, $l, $u) = get_bounds($sx, $sy, $target_row, $d);
+    if ($max - $min <= $d) {
+        $no_b_xs->{$_} = 1 for ($l..$u - 1);
+    }
+}
+
+my @nbx = keys %$no_b_xs;
+say scalar @nbx;
+
+sub distance {
+    my ($ax, $ay, $bx, $by) = @_;
+    return abs($ax - $bx) + abs($ay - $by);
+}
+
+sub get_bounds {
+    my ($x, $y1, $y2, $dist) = @_;
+    my ($min, $max) = sort { $a <=> $b } ($y1, $y2);
+    my $nd = $dist - ($max - $min);
+    my $lb = $x - $nd;
+    my $ub = $x + $nd;
+    return ($min, $max, $lb, $ub);
+}
+
---
+++ bin/day15-2.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/env perl
+use Mojo::Base -strict;
+
+use lib "../cheatsheet/lib";
+
+use Advent::Utils::Input qw(get_ints);
+
+use Array::Utils qw(:all);
+use List::AllUtils qw(:all);
+
+my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day15';
+$file = "inputs/day15-$file" if $file =~ /test/;
+
+my $fourM = 4000000;
+my $max_index = $file =~ /test/ ? 20 : $fourM;
+my $sensors = {};
+
+open(my $fh, '<', $file) or die $!;
+while (<$fh>) {
+    chomp;
+    my ($sx, $sy, $bx, $by) = get_ints($_, 1);
+    my $d = distance($sx, $sy, $bx, $by);
+    $sensors->{$sx, $sy} = $d;
+}
+
+# TODO: can we shrink this range?
+for my $y (0..$max_index) {
+    my @ranges = ();
+    while (my ($s, $d) = each %$sensors) {
+        my ($sx, $sy) = split $;, $s;
+        my ($min, $max, $u, $l) = get_bounds($sx, $sy, $y, $d);
+        if ($l >= 0 && $u <= $max_index) {
+            push @ranges, [max(0, $u), min($max_index, $l)];
+        }
+    }
+
+    @ranges = sort {
+        $a->[0] <=> $b->[0] || $a->[1] <=> $b->[1]
+    } @ranges;
+
+    my $max = 0;
+    for my $r (@ranges) {
+        my ($l, $u) = @$r;
+        # Only need to check that it's out of the range here
+        # otherwise you could move the beacon closer to the sensor and
+        # it would no longer be unique
+        if ($l > $max) {
+            say (($max + 1) * $fourM + $y);
+            exit;
+        }
+        $max = max($max, $u);
+    }
+}
+
+sub distance {
+    my ($ax, $ay, $bx, $by) = @_;
+    return abs($ax - $bx) + abs($ay - $by);
+}
+
+sub get_bounds {
+    my ($x, $y1, $y2, $dist) = @_;
+    my ($min, $max) = sort { $a <=> $b } ($y1, $y2);
+    my $nd = $dist - ($max - $min);
+    my $lb = $x - $nd;
+    my $ub = $x + $nd;
+    return ($min, $max, $lb, $ub);
+}
