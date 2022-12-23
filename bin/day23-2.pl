05bdbd63ffb3bc7b9192fd1fbb9c95abc65053ed
Owen Davies <owen@odavi.es>
2022-12-23 10:15:18 +0000

    Add day 23

---
+++ bin/day23-2.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/env perl
+use Mojo::Base -strict;
+
+use lib "../cheatsheet/lib";
+
+use Advent::Grid::Sparse;
+
+use List::AllUtils qw(:all);
+
+my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day23';
+$file = "inputs/day23-$file" if $file =~ /test/;
+
+my @DIRS = (
+    # N, NE, NW
+    [[0, -1], [1, -1], [-1, -1]],
+    # S, SE, SW
+    [[0, 1], [1, 1], [-1, 1]],
+    # W, NW, SW
+    [[-1, 0], [-1, -1], [-1, 1]],
+    # E, NE, SE
+    [[1, 0], [1, -1], [1, 1]],
+);
+
+my $elves = Advent::Grid::Sparse->new;
+my $width;
+my $height = 0;
+
+open(my $fh, '<', $file) or die $!;
+while (<$fh>) {
+    chomp;
+    $width = scalar $_ if !defined $width;
+    my $x = 0;
+    for my $ch (split //, $_) {
+        $elves->set($x, $height, 1) if $ch eq '#';
+        $x++;
+    }
+    $height++;
+}
+
+my $rounds = 1;
+
+while(1) {
+    my $to_move = {};
+    my $dests = {};
+    for my $elf ($elves->all()) {
+        my ($x, $y) = split $;, $elf;
+
+        next if all { !defined $_ } $elves->neighbour_values($x, $y);
+
+        for my $dir (@DIRS) {
+            my ($dx, $dy) = @{$dir->[0]};
+            my $empty = 1;
+
+            for my $check (@$dir) {
+                my ($ddx, $ddy) = @$check;
+                if (defined $elves->get($x + $ddx, $y + $ddy)) {
+                    $empty = 0;
+                    last;
+                }
+            }
+
+            if ($empty == 1) {
+                $to_move->{$x, $y} = [$x + $dx, $y + $dy];
+                push @{$dests->{$x + $dx, $y + $dy}}, [$x, $y];
+                last;
+            }
+        }
+    }
+
+    for my $dest (keys %$dests) {
+        my @prevs = @{$dests->{$dest}};
+        if (scalar @prevs > 1) {
+            delete $to_move->{$_->[0], $_->[1]} for @prevs;
+        }
+    }
+
+    if (scalar keys %$to_move == 0) {
+        say $rounds;
+        exit;
+    }
+
+    for my $tm (keys %$to_move) {
+        my ($fromx, $fromy) = split $;, $tm;
+        my ($tox, $toy) = @{$to_move->{$tm}};
+        $elves->delete($fromx, $fromy);
+        $elves->set($tox, $toy, 1);
+    }
+
+    my $d = shift @DIRS;
+    push @DIRS, $d;
+
+    $rounds++;
+}
+
