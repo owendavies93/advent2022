90f183735fc5afb0e0c8917213bd0451baf54f46
Owen Davies <owen@odavi.es>
2022-12-28 18:30:39 +0000

    Implement proper parsing in day 5

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## Advent of Code 2022
 
 ### TODO
-* Implement proper input parsing in Day 5
 * Do parsing and world building on day 14 in one step
 * Speed up
     * day 14 part 2
--- bin/day5-2.pl
+++ bin/day5-2.pl
@@ -4,35 +4,28 @@ use Mojo::Base -strict;
 my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day5';
 $file = "inputs/day5-$file" if $file =~ /test/;
 
-my $stacks = [
-    [],
-    ['Z','N'],
-    ['M','C','D'],
-    ['P'],
-];
-
-if ($file !~ /test/) {
-    $stacks = [
-        [],
-        ['W','B','D','N','C','F','J'],
-        ['P','Z','V','Q','L','S','T'],
-        ['P','Z','B','G','J','T'],
-        ['D','T','L','J','Z','B','H','C'],
-        ['G','V','B','J','S'],
-        ['P','S','Q'],
-        ['B','V','D','F','L','M','P','N'],
-        ['P','S','M','F','B','D','L','R'],
-        ['V','D','T','R'],
-    ];
-}
+my $stacks = [[]];
 
 open(my $fh, '<', $file) or die $!;
 while (<$fh>) {
     chomp;
-    my ($num, $from, $to) = $_ =~ /(\d+) from (\d) to (\d)/;
-    next if !defined $num;
-    my @removed = splice @{$stacks->[$from]}, $num * -1;
-    push @{$stacks->[$to]}, @removed;
+
+    if (substr($_, 0, 1) ne 'm') {
+        my $i = 1;
+        my @line = split //, $_;
+        while (my $c = $line[$i]) {
+            if ($c =~ /[A-Z]/) {
+                my $stack_idx = (($i - 1) / 4) + 1;
+                unshift @{$stacks->[$stack_idx]}, $c;
+            }
+            $i++;
+        }
+    } else {
+        my ($num, $from, $to) = $_ =~ /(\d+) from (\d) to (\d)/;
+        next if !defined $num;
+        my @removed = splice @{$stacks->[$from]}, $num * -1;
+        push @{$stacks->[$to]}, @removed;
+    }
 }
 
 say map { $_->[-1] // '' } @$stacks;
